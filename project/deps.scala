f1114f0590d6fc5801ac6ba9a9470372c76d730d
Alex Henning Johannessen <ahjohannessen@gmail.com>
2022-06-23 13:31:48 +0100

    Update deps.scala

--- project/deps.scala
+++ project/deps.scala
@@ -9,7 +9,7 @@ object deps {
     val scalaCheck      = "1.16.0"
     val munitDiscipline = "1.0.9"
     val munit           = "0.7.29"
-    val spikeCE         = "3.3.0"
+    val catsEffect      = "3.3.10"
 
   }
 
@@ -21,7 +21,7 @@ object deps {
 
   val munit           = "org.scalameta"  %% "munit"            % versions.munit
   val catsLaws        = "org.typelevel"  %% "cats-laws"        % versions.catsCore
-  val catsEffect      = "org.typelevel"  %% "cats-effect"      % versions.spikeCE
+  val catsEffect      = "org.typelevel"  %% "cats-effect"      % versions.catsEffect
   val scalaCheck      = "org.scalacheck" %% "scalacheck"       % versions.scalaCheck
   val munitDiscipline = "org.typelevel"  %% "discipline-munit" % versions.munitDiscipline
 
