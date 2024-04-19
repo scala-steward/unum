9b0e223957381706a8e4b4cb714dcc0573fca422
Scala Steward <scala_steward@virtuslab.com>
2024-04-19 13:16:37 +0000

    Update scalacheck to 1.18.0

--- project/deps.scala
+++ project/deps.scala
@@ -6,7 +6,7 @@ object deps {
   object versions {
 
     val catsCore        = "2.10.0"
-    val scalaCheck      = "1.17.1"
+    val scalaCheck      = "1.18.0"
     val munitDiscipline = "1.0.9"
     val munit           = "0.7.29"
 
