85c6cf280160773ff33a4fea0a9019cb4f821da3
Scala Steward <scala_steward@virtuslab.com>
2024-05-23 13:48:35 +0000

    Update munit to 1.0.0

--- project/deps.scala
+++ project/deps.scala
@@ -8,7 +8,7 @@ object deps {
     val catsCore        = "2.10.0"
     val scalaCheck      = "1.18.0"
     val munitDiscipline = "1.0.9"
-    val munit           = "0.7.29"
+    val munit           = "1.0.0"
 
   }
 
